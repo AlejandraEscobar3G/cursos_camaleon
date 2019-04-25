53a66def8b3ba731aa32911d05b6803a16cb07a1
alejandra.escobar <alejandra.escobar@santiago.mx>
2019-04-25 10:31:33 -0500

    Socket authentication (channels)

--- lib/cursos_web/controllers/auth.ex
+++ lib/cursos_web/controllers/auth.ex
@@ -1,46 +1,64 @@
 defmodule CursosWeb.Auth do
-    import Plug.Conn
-    import Phoenix.Controller
+  import Plug.Conn
+
+  import Phoenix.Controller
 
 
-    alias Cursos.Accounts
-    alias CursosWeb.Router.Helpers, as: Routes
+  alias Cursos.Accounts
+  alias CursosWeb.Router.Helpers, as: Routes
 
-    def init(opts), do: opts
+  def init(opts), do: opts
 
-    def call(conn, _opts) do
-        user_id = get_session(conn, :user_id)
-        user = user_id && Accounts.get_user(user_id)
-        assign(conn, :current_user, user)
+  def call(conn, _opts) do
+    user_id = get_session(conn, :user_id)
+
+    cond do
+      user = conn.assigns[:current_user] ->
+        put_current_user(conn, user)
+      
+      user = user_id && Accounts.get_user(user_id) ->
+        put_current_user(conn, user)
+      
+      true ->
+        assign(conn, :current_user, nil)           
     end
+  end
 
-    def login_user(conn, user) do
-        conn
-        |> assign(:current_user, user)
-        |> put_session(:user_id, user.id)
-        |> configure_session(renew: true)
-    end
+  def login_user(conn, user) do
+    conn
+    |> put_current_user(user)
+    |> put_session(:user_id, user.id)
+    |> configure_session(renew: true)
+  end
 
-    def login_by_username_and_pass(conn, username, password) do
-        case Accounts.authenticate_by_username_and_pass(username, password) do
-            {:ok, user} -> {:ok, login_user(conn, user)}
-            {:error, :unauthorized} -> {:error, :unauthorized, conn}
-            {:error, :not_found} -> {:error, :not_found, conn}
-        end
+  def login_by_username_and_pass(conn, username, password) do
+    case Accounts.authenticate_by_username_and_pass(username, password) do
+      {:ok, user} -> {:ok, login_user(conn, user)}
+      {:error, :unauthorized} -> {:error, :unauthorized, conn}
+      {:error, :not_found} -> {:error, :not_found, conn}
     end
+  end
 
-    def logout(conn) do
-        configure_session(conn, drop: true)
-    end
+  def logout(conn) do
+    configure_session(conn, drop: true)
+  end
 
-    def authenticate_user(conn, _opts) do
-        if conn.assigns.current_user do
-            conn
-        else
-            conn
-            |> put_flash(:error, "Debes iniciar sesión para poder acceder")
-            |> redirect(to: Routes.page_path(conn, :index))
-            |> halt()
-        end
+  def authenticate_user(conn, _opts) do
+    if conn.assigns.current_user do
+      conn
+    else
+      conn
+      |> put_flash(:error, "Debes iniciar sesión para poder acceder")
+      |> redirect(to: Routes.page_path(conn, :index))
+      |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
+  end
 end
