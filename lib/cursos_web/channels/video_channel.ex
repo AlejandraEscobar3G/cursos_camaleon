f89423986ae2e3b789aae93bce4b67cea0316842
alejandra.escobar <alejandra.escobar@santiago.mx>
2019-04-25 09:22:30 -0500

    joined to a channel

---
+++ lib/cursos_web/channels/video_channel.ex
@@ -0,0 +1,7 @@
+defmodule CursosWeb.VideoChannel do
+  use CursosWeb, :channel
+
+  def join("videos:" <> video_id, _params, socket) do
+    {:ok, assign(socket, :video_id, String.to_integer(video_id))}
+  end
+end
